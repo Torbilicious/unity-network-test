70a6fe0b34d45142e1089a0569b8a179ac315311
Torbilicious <torben.binder@googlemail.com>
2017-07-09 21:49:54 +0200

    Add mobile controls

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,27 +1,37 @@
 ﻿using UnityEngine;
 using UnityEngine.Networking;
+using UnityStandardAssets.CrossPlatformInput;
 
 public class PlayerController : NetworkBehaviour
 {
-    void Update()
+    private Rigidbody body;
+
+    void Start()
     {
-        if (!isLocalPlayer)
-        {
-            return;
-        }
-
-        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
-        var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
-
-        transform.Rotate(0, x, 0);
-        transform.Translate(0, 0, z);
-        
-        if (Input.GetKeyDown ("space")) {
-            Vector3 up = transform.TransformDirection (Vector3.up);
-            GetComponent<Rigidbody>().AddForce (up * 5, ForceMode.Impulse);
-        }
+        body = GetComponent<Rigidbody>();
     }
     
+    void Update()
+    {
+        if (!isLocalPlayer) return;
+
+        var x = CrossPlatformInputManager.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+        var z = CrossPlatformInputManager.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+        
+        transform.Rotate(0, x, 0);
+        transform.Translate(0, 0, z);
+    }
+
+    private void FixedUpdate()
+    {
+        if (!isLocalPlayer) return;
+        
+        if (Input.GetKeyDown ("space") || CrossPlatformInputManager.GetButtonDown("Jump")) {
+            Vector3 up = transform.TransformDirection (Vector3.up);
+            body.AddForce (up * 5, ForceMode.Impulse);
+        }
+    }
+
     public override void OnStartLocalPlayer()
     {
         Camera.main.GetComponent<CameraFollow>().setTarget(gameObject.transform);
