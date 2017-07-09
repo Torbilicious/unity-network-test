62b6570a85edf86464194f6b297d3ee025d14a04
Markellus <gmx@marcel-bulla.de>
2017-07-09 20:10:48 +0200

    Fixed camera following

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -3,22 +3,38 @@
  public class CameraFollow : MonoBehaviour
  {
      public Transform playerTransform;
-     public int depth = -20;
      private Vector3 offset;
-     
-     // Update is called once per frame
-     void Update()
-     {
-         if(playerTransform != null)
-         {
-//             transform.position = playerTransform.position + new Vector3(0,10,depth);
-             transform.position = new Vector3(
-                 playerTransform.position.x,
-                 playerTransform.position.y + 5,
-                 playerTransform.position.z - 10);
 
-             transform.rotation = playerTransform.rotation;
-         }
+    public float distance = 5;
+    public float height = 5;
+
+    private Vector3 playerPrevPos, playerMoveDir;
+
+    // Use this for initialization
+    void Start()
+    {
+        playerPrevPos = playerTransform.position;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+     {
+        if (playerTransform != null)
+        {
+            playerMoveDir = playerTransform.position - playerPrevPos;
+
+            if (playerMoveDir != Vector3.zero)
+            {
+                playerMoveDir.Normalize();
+                transform.position = playerTransform.position - playerMoveDir * distance;
+                transform.position = transform.position + new Vector3(0, height, 0);
+
+                transform.LookAt(playerTransform.position);
+
+                playerPrevPos = playerTransform.position;
+            }
+        }
      }
  
      public void setTarget(Transform target)
