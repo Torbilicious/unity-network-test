cb04b58c657fb6bc4db75e557dad56cd0e2064fe
Torbilicious <torben.binder@googlemail.com>
2017-07-10 20:06:59 +0200

    Add a few surroundings to play with

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,8 +5,11 @@ using UnityStandardAssets.CrossPlatformInput;
 
 public class PlayerController : NetworkBehaviour
 {
+    public float movementSpeed = 5.0f;
+
     private Rigidbody body;
     private RaycastHit hit;
+    private float turnRate = 150.0f;
 
     void Start()
     {
@@ -18,7 +21,7 @@ public class PlayerController : NetworkBehaviour
         if (!isLocalPlayer) return;
 
         var x = CrossPlatformInputManager.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
-        var z = CrossPlatformInputManager.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+        var z = CrossPlatformInputManager.GetAxis("Vertical") * Time.deltaTime * 5.0f;
         
         transform.Rotate(0, x, 0);
         transform.Translate(0, 0, z);
